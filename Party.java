16852d668311c6c37861b9c2fb88a96232fd8b88
gabdele <157085723+gabdele@users.noreply.github.com>
2024-02-26 22:30:11 -0500

    Create Party.java

---
+++ Party.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Party {
+    private int guests;
+
+    public int getGuests(){
+        return guests;
+    }
+    public void setGuests(int x){
+        this.guests = x;
+    }
+    public void DisplayInvitation(){
+        JOptionPane.showMessageDialog(null,
+                "Please come to my party!","please!"
+                ,JOptionPane.WARNING_MESSAGE); //customizes the dialog
+    }
+}
